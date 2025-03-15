5e810d34fa05a6a424b601a8a1b0b97f1a1a312b
pavan <124360829+BhimaPavanTeja@users.noreply.github.com>
2025-03-15 15:48:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ require("./db/connection");
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "http://localhost:3002",
-  "http://localhost:3003",
-  "http://localhost:3004",
-  "http://localhost:3005",
-  "http://localhost:3006",
-  "http://localhost:3007",
   "https://whatsapp-clone-ted.web.app",
 ];
 
